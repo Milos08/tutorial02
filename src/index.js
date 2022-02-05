c5fd962f3e2fb7dbb51da517a5d60cf0835538cc
Milos08 <milosbuha98@gmail.com>
2022-02-05 16:09:13 +0100

    comment section

--- src/index.js
+++ src/index.js
@@ -1,27 +1,33 @@
 import react from "react";
 import reactDom from "react-dom";
 import faker from "faker";
-
+import CommentDetail from './CommentDetail';
 import './index.css';
 
 const App = () => {
-    console.log(faker)
     return (
         <div className="ui contanier comments">
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar" src={faker.image.image()} />
-                </a>
-                <div className="content">
-                    <a href="/" className="author">
-                        Sadin
-                    </a>
-                    <div className="metadata">
-                        <span className="date">Today at 6:00PM</span>
-                    </div>
-                    <div className="text">Nice blog post!</div>
-                </div>
-            </div>
+            <CommentDetail 
+            author = "Sadin" 
+            timeAgo = "Today at 4:45PM" 
+            text = "ONA JE" 
+            avatar ={faker.image.avatar()}
+            />
+
+            <CommentDetail 
+            author = "Losma" 
+            timeAgo = "Today at 2:00AM" 
+            text = "UBILILA" 
+            avatar ={faker.image.avatar()}
+            />
+
+            <CommentDetail 
+            author = "Maki" 
+            timeAgo = "Yesterday at 5:00PM" 
+            text = "MOJE SVE" 
+            picture ={faker.image.avatar()}
+            />
+
         </div>
     );
 };
