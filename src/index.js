969f99683e258fcd3101f7c18236ccd9918a52f9
Milos08 <milosbuha98@gmail.com>
2022-02-05 17:24:26 +0100

    Comment section

--- src/index.js
+++ src/index.js
@@ -3,30 +3,37 @@ import reactDom from "react-dom";
 import faker from "faker";
 import CommentDetail from './CommentDetail';
 import './index.css';
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
     return (
         <div className="ui contanier comments">
-            <CommentDetail 
-            author = "Sadin" 
-            timeAgo = "Today at 4:45PM" 
-            text = "ONA JE" 
-            avatar ={faker.image.avatar()}
-            />
+            <ApprovalCard> 
+                <CommentDetail 
+                author = "Sadin" 
+                timeAgo = "Today at 4:45PM" 
+                text = "ONA JE" 
+                avatar ={faker.image.avatar()}
+                />
+            </ApprovalCard>
 
-            <CommentDetail 
-            author = "Losma" 
-            timeAgo = "Today at 2:00AM" 
-            text = "UBILILA" 
-            avatar ={faker.image.avatar()}
-            />
+            <ApprovalCard>
+                <CommentDetail 
+                author = "Losma" 
+                timeAgo = "Today at 2:00AM" 
+                text = "UBILA" 
+                avatar ={faker.image.avatar()}
+                />
+            </ApprovalCard>
 
-            <CommentDetail 
-            author = "Maki" 
-            timeAgo = "Yesterday at 5:00PM" 
-            text = "MOJE SVE" 
-            picture ={faker.image.avatar()}
-            />
+            <ApprovalCard>
+                <CommentDetail 
+                author = "Maki" 
+                timeAgo = "Yesterday at 5:00PM" 
+                text = "MOJE SVE" 
+                picture ={faker.image.avatar()}
+                />
+            </ApprovalCard>
 
         </div>
     );
